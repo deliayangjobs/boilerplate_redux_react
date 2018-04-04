75748c94fa86a448faa4132b98365b8144f4b79a
Delia Yang <deliay@surveymonkey.com>
2018-04-04 13:35:47 -0700

    actions

---
+++ redux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: the good part', pages: 101},
+        { title: 'Harry Potter', pages: 309},
+        { title: 'The Dark Tower', pages: 10},
+        { title: 'Eloquent ruby', pages:19}
+    ];
+}
