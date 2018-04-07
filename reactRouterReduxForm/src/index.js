940726e758075287fc7cc4444f6dba1fd2af5165
Delia Yang <deliay@surveymonkey.com>
2018-04-06 23:36:48 -0700

    react router switch

--- reactRouterReduxForm/src/index.js
+++ reactRouterReduxForm/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route }from 'react-router-dom';
+import { BrowserRouter, Route, Switch }from 'react-router-dom';
 import ReduxPromise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
@@ -14,7 +15,13 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
         <div>
+            <Switch>
+            // go to /posts/new will see both components, since it matches both paths
+            // react router does lose match here, solution is use Switch
+            // Switch render only the first route matches the url, so put / at last
+            <Route path="/posts/new" component={PostsNew} />
             <Route path="/" component={PostsIndex} />
+            </Switch>
         </div>
     </BrowserRouter>
   </Provider>
