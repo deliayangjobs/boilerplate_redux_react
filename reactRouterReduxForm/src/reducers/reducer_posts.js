ab4bf712e1d4a39b3b1bf132fc13e6d7a617005b
Delia Yang <deliay@surveymonkey.com>
2018-04-08 21:30:52 -0700

    delete post

--- reactRouterReduxForm/src/reducers/reducer_posts.js
+++ reactRouterReduxForm/src/reducers/reducer_posts.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state={}, action) {
     switch(action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);
+        case FETCH_POST:
+            const post = action.payload.data;
+            // not create array here, [] is key interpretion, put id as key
+            return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
             // lodash mapKeys function args [{id:1, name:'dd'}, ...]
             // return {"1": {the whole object}, ...}
